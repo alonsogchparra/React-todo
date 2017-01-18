c8a2cd3402390b597f627a42ff209cb1262c1ef8
aliens9889 <alienx9889@gmail.com>
2017-01-18 16:55:10 -0400

    feat(AddTodo): AddTodo Components added

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
+
 var TodoApp = React.createClass({
     getInitialState: function () {
         return {
@@ -20,11 +22,15 @@ var TodoApp = React.createClass({
             ]
         }
     },
+    handleAddTodo: function (text) {
+      alert('new todo:' + text);
+    },
     render: function () {
         var{todos} = this.state;
         return (
             <div>
                 <TodoList todos={todos}/>
+                <AddTodo onAddTodo={this.handleAddTodo()}/>
             </div>
         )
     }
