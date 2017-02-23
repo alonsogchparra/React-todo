dfd974a868cd6a591df8f58c19c1d965d6cd6e96
aliens9889 <alienx9889@gmail.com>
2017-02-22 23:23:35 -0400

    feat(testing): Add showCompleted and searchText reducers

---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,32 @@
+var expect = require('expect');
+var df = require('deep-freeze-strict');
+
+var reducers = require('reducers');
+
+describe('Reducers', () => {
+    describe('searchTextReducer', () => {
+        it('should set searchText', () => {
+            var action = {
+                type: 'SET_SEARCH_TEXT',
+                searchText: 'dog'
+        }
+            ;
+            var res = reducers.searchTexReducer(df(''), df(action));
+
+            expect(res).toEqual(action.searchText);
+        });
+    });
+
+
+    describe('showCompletedReducer', () => {
+       it('should toggle showCompleted', () => {
+           var action = {
+               type: 'TOGGLE_SHOW_COMPLETED'
+           };
+           var res = reducers.showCompletedReducer(df(false), df(action));
+
+           expect(res).toEqual(true);
+       });
+    });
+
+});
